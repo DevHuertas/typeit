1c283ccd91affb2afe66c05ba5634a8f2b4cd275
aallfredo <alfredo.e.alvarez@gmail.com>
2013-03-24 09:38:48 -0700

    Adding support for mit.soddio for stories.

---
+++ Dialer/TypeItDialerMechanics/DialerRole/Models/AvailableMaterials.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DialerRole.Models
+{
+    public class AvailableMaterials
+    {
+        public List<string> GetTop3MaterialsForId(int userId)
+        {
+            //TODO:Load this from a database
+            return new List<string>(){"Verb" , "Emotions" , "Story"};
+        }
+    }
+}
